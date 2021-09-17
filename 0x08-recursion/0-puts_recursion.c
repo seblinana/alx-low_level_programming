08fa97c25241355d050154d012f2664ba208a232
seblewongel petros <seblinana25@gmail.com>
2021-09-17 07:50:27 +0300

    update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
-#include "main.h"
+#include "main.h" 
 
 /**
  * _puts_recursion-prints a string, followed by a new line
  * @s: string to print
- * /
+ * */
  void _puts_recursion(char *s)
  {
      if (*s == '\0')
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 
 /**
  * _print_rev_recursion - prints a string in reverse
